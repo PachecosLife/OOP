f31f17f9e2d00aa6b1b78185f1b1a6994edee967
i5 <i5@Pachecos>
2021-11-16 06:56:56 -0300

    POO - Controle Remoto c/ Interface

---
+++ Controle Remoto/src/ProgramaPrincipal.java
@@ -0,0 +1,15 @@
+
+	public class ProgramaPrincipal {
+	
+		public static void main(String[] args) {
+		
+		ControleRemoto c = new ControleRemoto();
+		
+		c.ligar();
+		c.abrirMenu();
+		c.fecharMenu();
+			
+		
+	}
+
+}
